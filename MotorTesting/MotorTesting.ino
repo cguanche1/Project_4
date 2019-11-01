d0c1e72c20bb1df0860b49ff523e2941d87ec51a
cguanche1 <carlos.guanche@yale.edu>
2019-11-01 02:16:18 -0400

    Updated and extended motor testing

--- MotorTesting/MotorTesting.ino
+++ MotorTesting/MotorTesting.ino
@@ -1,22 +1,34 @@
 #include <Stepper.h>
 #include <ESP32Servo.h>
 
-int stepsPerRev = 2048;
+int stepsPerRev = 4096;
 
 Stepper mystepper(stepsPerRev, 32,33,25,26);
 
 Servo myServo;
 
-int pos = 0;
-int posStep = 10;
+int pos = 1;
+int posStep = 1;
 
 int servoPin = 13;
 
 int stepCount = 0;
+int turn = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(32, OUTPUT);
+  pinMode(33, OUTPUT);
+  pinMode(25, OUTPUT);
+  pinMode(27, OUTPUT);
+  
 
+  pinMode(13, OUTPUT);
+
+  pinMode(35, INPUT_PULLUP);
+
+  pinMode(26, OUTPUT);
+  
   myServo.setPeriodHertz(50);
   myServo.attach(servoPin);
 
@@ -29,13 +41,38 @@ void loop() {
   
   mystepper.step(1); //1/360th of a rev
   
-  stepCount = (stepCount + 1) % 2048;
+  stepCount = (stepCount + 1) % 4096;
 
   delay(5);
 
 
+  if(pos == 180){
+    posStep = -1 * posStep;
+  }
+  if(pos == 0){
+    posStep = -1 * posStep;
+  }
+
+  pos = pos + posStep;
+  myServo.write(pos);
+
+  if(digitalRead(35) == LOW){
+    delay(15);
+    if(digitalRead(35) == LOW){
+      analogWrite(26, 125);
+    }
+  }
+
+  if(digitalRead(35) == HIGH){
+    delay(10);
+    if(digitalRead(35) == HIGH){
+      analogWrite(26, 0);
+    }
+  }
   
-  if(stepCount == 2047){
+  
+  /*
+  if(stepCount == 4095){
     pos = pos + posStep;
     myServo.write(pos);
     if(pos == 180){
@@ -45,5 +82,6 @@ void loop() {
       posStep = posStep * -1;
     }
   }
+  */
   //delay(5);
 }
